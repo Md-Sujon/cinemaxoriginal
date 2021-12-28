db9e9b80a39d6bf8127538bd913b576c5e5b2647
Md-Sujon <mdsujon829562@gmail.com>
2021-12-28 15:55:15 +0600

    navbar header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Button, Carousel } from 'react-bootstrap';
+import './Header.css';
+
+const Header = () => {
+    return (
+     
+    <Carousel className="carousel">
+  <Carousel.Item interval={1000} >
+    <img
+      className="d-block img"
+      src="https://snagfilms-a.akamaihd.net/dd078ff5-b16e-45e4-9723-501b56b9df0a/images/2021/08/12/1628770036394_unoloukikposter3840x10804thepcopy_32x9Images.jpg?impolicy=resize&w=1536&h=432"
+      alt="First slide"
+    />
+    <Carousel.Caption className="custom-foot">
+      <h3>Unoloukik</h3>
+      <h6>Psychological Drama | 5 Episodes</h6>
+
+      <Button className="WatchButton"><h5>Watch Now</h5></Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+
+  <Carousel.Caption>
+      <h3>Morichika</h3>
+      <h6>Crime Thriller | 8 Episodes</h6>
+      <Button className="WatchButton"><h5>Watch Now</h5></Button>
+    </Carousel.Caption>
+
+    <img
+      className="d-block w-100"
+      src="https://snagfilms-a.akamaihd.net/dd078ff5-b16e-45e4-9723-501b56b9df0a/images/2021/07/12/1626098789510_morichika3840px1080lastversion_32x9Images.jpg?impolicy=resize&w=1536&h=432"
+      alt="Second slide"
+    />
+   
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://snagfilms-a.akamaihd.net/dd078ff5-b16e-45e4-9723-501b56b9df0a/images/2021/11/18/1637236411430_329_32x9Images.jpg?impolicy=resize&w=1536&h=432"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Sugar Free</h3>
+      <h6>Comedy Drama | 4 Episodes</h6>
+      
+      <Button className="WatchButton"><h5>Watch Now</h5></Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://snagfilms-a.akamaihd.net/dd078ff5-b16e-45e4-9723-501b56b9df0a/images/2021/12/23/1640265386986_3840x1080_32x9Images.jpg?impolicy=resize&w=1536&h=432"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Jaago Bahey</h3>
+      <h6>Historical Drama | Episodes</h6>
+      <Button className="WatchButton"><h5>Watch Now</h5></Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://snagfilms-a.akamaihd.net/dd078ff5-b16e-45e4-9723-501b56b9df0a/images/2021/08/12/1628770036394_unoloukikposter3840x10804thepcopy_32x9Images.jpg?impolicy=resize&w=1536&h=432"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Five slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <Button className="WatchButton"><h5>Watch Now</h5></Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+
+{/* <Carousel.Item className="carousel-button">
+
+</Carousel.Item> */}
+      
+</Carousel>
+    
+    );
+};
+
+export default Header;
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import  './Navbar.css';
+import { Button, Container, Form, FormControl, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import image from '../../image/cinemax.jpg'
+// import { Modal } from 'bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
+const HeaderNav = () => {
+
+//   const [show, setShow] = useState(false);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+    return (
+        <div className="color">
+        <Navbar className="navbarStyle" active collapseOnSelect expand="lg">
+  <Container>
+  <Navbar.Brand href="#home">
+      <Image className="logo-image" src={image}/>
+      
+      
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features">HOME</Nav.Link>
+     
+      <NavDropdown className="navDropdown" title="MOVIES" id="collasible-nav-dropdown">
+        <NavDropdown.Item className="navDropdown" href="#action/3.1">ALL MOVIES</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">ROMANTIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">ACTION</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">THRILLER</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">DRAMATIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">COMEDY</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">INSPIRATIONAL</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">PATRIOTIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">INDIE</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">FAMILY</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">CHILDREN</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">WOMEN CENTRIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">CLASSIC</NavDropdown.Item>
+      </NavDropdown>
+
+    
+      <NavDropdown title="SERIES" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">ALL SERIES</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">ROMANTIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">DRAMATIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">COMEDY</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">FAMILY</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">CLASSIC</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">THRILLER</NavDropdown.Item>
+      </NavDropdown>
+      
+
+
+
+      <NavDropdown title="SHOWS" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">ALL NON-FICTION</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">MUSIC VIDEO</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">DOCUMENTARY</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">OTHER SHOWS</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">FAMILY</NavDropdown.Item>
+      </NavDropdown>
+
+      <Nav.Link href="#features">REDEEM</Nav.Link>
+      
+    </Nav>
+    <Nav>
+      
+    {/* <Button variant="outline-danger">LOG IN</Button>
+        <Button variant="outline-danger"></Button> */}
+        <Nav.Link href="#Search"><Button className="SearchButton" variant="outline-danger"><FontAwesomeIcon icon={faSearch}/></Button></Nav.Link>
+      <Nav.Link className="subscribeButton" href="#SUBSCRIBE NOW"><small>SUBSCRIBE NOW</small></Nav.Link>
+      <Nav.Link href="#deets"><Button className="loginButton" variant="outline-danger">LOG IN</Button></Nav.Link>
+      
+      {/* <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link> */}
+    </Nav>
+    
+
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+{/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal> */}
+
+
+  </div>
+    );
+};
+
+export default HeaderNav;
